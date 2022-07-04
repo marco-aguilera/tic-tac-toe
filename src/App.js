5f641f28e0a83f0e320ae5c1cc18f76b131df631
Marco Aguilera <marco.aguilera.guerra@gmail.com>
2022-07-04 01:56:23 -0500

    Adding the Source Folder that the tic tac toe game lives inside of for React.js.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import TicTacToe from './TicTacToe/tictactoe.js' //Must plug in the file type for the import to work
+
+function App() {
+  return (
+    <div className="App">
+      
+     <TicTacToe/> 
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/TicTacToe/tictactoe.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const tictactoe = () => {
+  return (
+    
+    <h1>Tic Tac Toe</h1>
+
+  )
+}
+
+export default tictactoe
