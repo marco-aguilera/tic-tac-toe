75a9e52180b2d428bed76c0cd97eb7215e6e9088
Marco Aguilera <marco.aguilera.guerra@gmail.com>
2022-07-05 23:22:00 -0500

    Adding a 3 by 3 grid to start the build for the game

--- src/TicTacToe/tictactoe.js
+++ src/TicTacToe/tictactoe.js
@@ -1,9 +1,34 @@
 import React from 'react'
 
 const tictactoe = () => {
+  const Cell = () => {
+    return <td>-</td>
+  }
   return (
     
-    <h1>Tic Tac Toe</h1>
+  //<h1>Tic Tac Toe</h1> 
+  <div> 
+    <table>
+      <tbody>
+          <tr>
+            <Cell/>
+            <Cell/>
+            <Cell/>
+          </tr>
+          <tr>
+            <Cell />
+            <Cell />
+            <Cell />
+          </tr>
+          <tr>
+            <Cell />
+            <Cell />
+            <Cell />
+          </tr>
+      </tbody>
+    </table>
+  </div>
+    
 
   )
 }
