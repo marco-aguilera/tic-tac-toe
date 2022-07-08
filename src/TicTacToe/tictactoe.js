e675ba5ce02d16bfa39c6966acfb6276adf778e0
Marco Aguilera <marco.aguilera.guerra@gmail.com>
2022-07-08 11:21:33 -0500

    Centered the Cells and added a click to the cells with a value to represent each cell

--- src/TicTacToe/tictactoe.js
+++ src/TicTacToe/tictactoe.js
@@ -1,29 +1,35 @@
 import React from 'react'
 import './tictactoe.css'
+
 const tictactoe = () => {
-  const Cell = () => {
-    return <td>-</td> //css: table td will place a box around it.
+
+  const handleClick = (num) => { //adding a click for the individual cells 
+    alert(num);
   }
+
+  const Cell = ({num}) => { //num is being passed in a a prop so we need to add brackets
+    return <td onClick={() => handleClick (num)}>-</td> //added a onclick func for the cells 
+  }
+
   return (
-      
   //Setting up the table grid for the game 3x3
-  <div> 
+  <div className='container' > 
     <table> 
       <tbody>
           <tr> 
-            <Cell/> 
-            <Cell/>
-            <Cell/>
+            <Cell num={0} /> 
+            <Cell num={1} />
+            <Cell num={2} />
           </tr>
           <tr>
-            <Cell />
-            <Cell />
-            <Cell />
+            <Cell num={3} />
+            <Cell num={4} />
+            <Cell num={5} />
           </tr>
           <tr>
-            <Cell />
-            <Cell />
-            <Cell />
+            <Cell num={6} />
+            <Cell num={7} />
+            <Cell num={8} />
           </tr>
       </tbody>
     </table>
