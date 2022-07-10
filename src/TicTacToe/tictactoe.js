c4e7937a30b0dcd69bfd5fcfc5a567370686ad57
Marco Aguilera <marco.aguilera.guerra@gmail.com>
2022-07-10 13:57:14 -0500

    Added the array for the cells and winning combonations

--- src/TicTacToe/tictactoe.js
+++ src/TicTacToe/tictactoe.js
@@ -4,20 +4,56 @@ import './tictactoe.css'
 const TicTacToe = () => {  //React component names must start with an uppercase letter.
 
   const [turn, setTurn] = useState('x'); // tracking alternating turns
+  const [cells, setCells] = useState(Array(9).fill(''))
 
   const handleClick = (num) => { //adding a click for the individual cells 
-   alert(num);
+   //alert(num); //alerts which number you are on in the cell (Not needed)
+
+
+    let squares = [...cells];
+
+    const checkForWinner = (squares) => {
+      let combos = {
+        across: [
+          [0,1,2],
+          [3,4,5],
+          [6,7,8],
+        ],
+        down: [
+          [0,3,6],
+          [1,4,7],
+          [6,5,8],
+        ],
+        dialog: [
+          [0,4,8],
+          [2,4,6],
+        ],
+      };
+
+      
+
+    } 
+
+    if(cells[num] !== '') {  //stops the users from clickinig a cell that is already populated.
+      alert('already clicked!');
+      return;
+    }
 
     if(turn === 'x') {
+      squares[num] = 'x';
       setTurn('o');
     } else {
+      squares[num] = 'o';
       setTurn('x');
     }
+
+    checkForWinner(squares);
+    setCells(squares); //update the state variable and keep that item o or x inside of the array
 
   };
 
   const Cell = ({num}) => { //num is being passed in a a prop so we need to add brackets
-    return <td onClick={() => handleClick (num)}>-</td> //added a onclick func for the cells 
+    return <td onClick={() => handleClick (num)}>{cells[num]}</td> //added a onclick func for the cells 
   }
 
   return (
